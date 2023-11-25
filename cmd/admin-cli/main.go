49a391d2ec485676c30470d8447af4537f298184
chirikova <t.desyatkina@gmail.com>
2023-11-25 16:21:24 +0300

    admin-cli

--- cmd/admin-cli/main.go
+++ cmd/admin-cli/main.go
@@ -10,141 +10,215 @@ import (
 
 	"antibruteforce/internal/transport/grpc"
 	"antibruteforce/internal/transport/grpc/api"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
+var address string
+var timeout time.Duration
+
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	client, err := grpc.NewClient(ctx, address)
+	if err != nil {
+		log.Fatalf("unable to establish connection: %s", err)
+	}
+
 	app := &cli.App{
 		Name: "CLI admin panel",
 		Usage: `интерфейс для ручного администрирования сервиса. 
 		Через CLI есть возможность вызвать сброс бакета и управлять whitelist/blacklist-ом. 
-		CLI рабоатет через GRPC интерфейс`,
+		CLI работает через GRPC интерфейс`,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:  "address",
-				Value: "localhost:9095",
-				Usage: "Anti-bruteforce server address? e.g.: 127.0.0.1:9091",
+				Name:        "address",
+				Value:       "localhost:9095",
+				Usage:       "Anti-bruteforce server address? e.g.: 127.0.0.1:9091",
+				Destination: &address,
 			},
 			&cli.DurationFlag{
-				Name:  "connect-timeout",
-				Value: 3 * time.Second,
-				Usage: "Anti-bruteforce client connection timeout",
+				Name:        "connect-timeout",
+				Value:       300 * time.Second,
+				Usage:       "Anti-bruteforce client connection timeout",
+				Destination: &timeout,
 			},
 		},
-		Commands: []cli.Command{
+		Commands: []*cli.Command{
 			{
 				Name:    "Reset",
 				Aliases: []string{"reset"},
 				Usage:   "Reset bucket by login and ip",
 				Flags: []cli.Flag{
 					&cli.StringFlag{
-						Name:  "login",
-						Usage: "Login",
+						Name:     "login",
+						Usage:    "Login, e.g.: sidorov",
+						Required: true,
 					},
 					&cli.StringFlag{
-						Name:  "ip",
-						Usage: "Ip",
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
 					},
 				},
 				Action: func(c *cli.Context) error {
-					ctx, cancel := context.WithTimeout(context.Background(), c.Duration("connect-timeout"))
-					defer cancel()
-
-					client, err := grpc.NewClient(ctx, c.String("address"))
-					if err != nil {
-						log.Fatalf("unable to establish connection: %s", err)
-					}
-
-					if response, err := client.Reset(ctx, &api.ResetRequest{}); err != nil {
+					r := api.ResetRequest{Login: c.String("login"), Ip: c.String("ip")}
+					if response, err := client.Reset(ctx, &r); err != nil {
 						log.Println("Error: ", err)
+						return err
 					} else {
 						log.Printf("Response: %v\n", response)
 					}
 
-					go func() {
-						terminate := make(chan os.Signal, 1)
-						signal.Notify(terminate, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
-						<-terminate
-						log.Println("Received system interrupt...")
-						cancel()
-					}()
-
-					return nil
-				},
-			},
-			{
-				Name:    "Auth",
-				Aliases: []string{"auth"},
-				Usage:   "Authorization attempt",
-				Action: func(c *cli.Context) error {
 					return nil
 				},
 			},
 			{
 				Name:    "Add to whitelist",
-				Aliases: []string{"add2whitelist"},
+				Aliases: []string{"aw"},
 				Usage:   "add subnet(ip + mask) to the whitelist (e.g.: 255.0.0.0/12)",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
+					},
+				},
 				Action: func(c *cli.Context) error {
+					r := api.WhitelistAddRequest{
+						SubNet: &api.SubNet{
+							Ip:   c.String("ip"),
+							Mask: c.String("mask"),
+						},
+					}
+					if response, err := client.WhitelistAdd(ctx, &r); err != nil {
+						log.Println("Error: ", err)
+						return err
+					} else {
+						log.Printf("Response: %v\n", response)
+					}
+
 					return nil
 				},
 			},
 			{
-				Name:    "remove_from_whitelist",
+				Name:    "Remove from whitelist",
 				Aliases: []string{"rw"},
 				Usage:   "remove subnet(ip + mask) from the whitelist. For example: 192.168.130.0/24",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
+					},
+				},
 				Action: func(c *cli.Context) error {
+					r := api.WhitelistRemoveRequest{
+						SubNet: &api.SubNet{
+							Ip:   c.String("ip"),
+							Mask: c.String("mask"),
+						},
+					}
+					if response, err := client.WhitelistRemove(ctx, &r); err != nil {
+						log.Println("Error: ", err)
+						return err
+					} else {
+						log.Printf("Response: %v\n", response)
+					}
+
 					return nil
 				},
 			},
 			{
-				Name:    "add_in_blacklist",
+				Name:    "Add to blacklist",
 				Aliases: []string{"ab"},
 				Usage:   "add subnet(ip + mask) in the blacklist. For example: 192.168.130.0/24",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
+					},
+				},
 				Action: func(c *cli.Context) error {
+					r := api.BlacklistAddRequest{
+						SubNet: &api.SubNet{
+							Ip:   c.String("ip"),
+							Mask: c.String("mask"),
+						},
+					}
+					if response, err := client.BlacklistAdd(ctx, &r); err != nil {
+						log.Println("Error: ", err)
+						return err
+					} else {
+						log.Printf("Response: %v\n", response)
+					}
+
 					return nil
 				},
 			},
 			{
-				Name:    "remove_from_blacklist",
+				Name:    "Remove from blacklist",
 				Aliases: []string{"rb"},
 				Usage:   "remove subnet(ip + mask) from the blacklist. For example: 192.168.130.0/24",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "ip",
+						Usage:    "Ip, e.g.: 192.168.0.1",
+						Required: true,
+					},
+				},
 				Action: func(c *cli.Context) error {
+					r := api.BlacklistRemoveRequest{
+						SubNet: &api.SubNet{
+							Ip:   c.String("ip"),
+							Mask: c.String("mask"),
+						},
+					}
+					if response, err := client.BlacklistRemove(ctx, &r); err != nil {
+						log.Println("Error: ", err)
+						return err
+					} else {
+						log.Printf("Response: %v\n", response)
+					}
+
 					return nil
 				},
 			},
 		},
 	}
 
-	err := app.Run(os.Args)
+	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-//type clientResponse func(client api.ApiServiceClient) (string, error)
-//
-//func runCommand(clientResponse clientResponse, cfg config.Config) error {
-//	ctx, cancel := context.WithTimeout(context.Background(), cfg.GRPC.Client.ConnectionTimeout)
-//	defer cancel()
-//
-//	client, err := grpc.NewClient(ctx, cfg.GRPC)
-//	if err != nil {
-//		log.Fatalf("unable to establish connection: %s", err)
-//	}
-//
-//	if response, err := clientResponse(client); err != nil {
-//		log.Println("Error: ", err)
-//	} else {
-//		log.Printf("Response: %v\n", response)
-//	}
-//
-//	go func() {
-//		terminate := make(chan os.Signal, 1)
-//		signal.Notify(terminate, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
-//		<-terminate
-//		log.Println("Received system interrupt...")
-//		cancel()
-//	}()
-//
-//	return nil
-//}
+	terminate := make(chan os.Signal, 1)
+	signal.Notify(terminate, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	go func() {
+		<-terminate
+
+		log.Println("Received system interrupt...")
+		cancel()
+	}()
+}
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/golang/protobuf v1.5.3
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.8.4
-	github.com/urfave/cli v1.22.14
+	github.com/urfave/cli/v2 v2.25.7
 	google.golang.org/grpc v1.59.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -16,6 +16,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
